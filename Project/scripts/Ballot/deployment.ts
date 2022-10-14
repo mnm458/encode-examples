df80fbe099c775b373ede4c32dd0dc4eb56491fb
mnm458 <mohaknainmalhotra@gmail.com>
2022-10-14 19:19:38 +1100

    deployment

--- Project/scripts/Ballot/deployment.ts
+++ Project/scripts/Ballot/deployment.ts
@@ -6,6 +6,11 @@ const EXPOSED_KEY = "";
 function convertStringArrayToBytes32(array: string[]){
   const bytes32Array = [];
   for (let index = 0; index < array.length; index++){
-    
+    bytes32Array.push(ethers.utils.formatBytes32String(array[index]));
   }
+  return bytes32Array;
+}
+
+async function main(){
+  
 }
