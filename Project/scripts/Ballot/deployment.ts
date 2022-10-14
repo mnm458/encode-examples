35953bbd13a746a9c8da1ada570fead49f701493
mnm458 <mohaknainmalhotra@gmail.com>
2022-10-14 19:18:43 +1100

    deployment

--- Project/scripts/Ballot/deployment.ts
+++ Project/scripts/Ballot/deployment.ts
@@ -4,5 +4,8 @@ import "dotenv/config";
 const EXPOSED_KEY = "";
 
 function convertStringArrayToBytes32(array: string[]){
-  
+  const bytes32Array = [];
+  for (let index = 0; index < array.length; index++){
+    
+  }
 }
