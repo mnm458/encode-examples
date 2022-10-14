25b06fe4ce4bd4d56f8240f516a5cc293b34e05c
mnm458 <mohaknainmalhotra@gmail.com>
2022-10-15 02:36:06 +1100

    erc20

--- Project/contracts/MyToken.sol
+++ Project/contracts/MyToken.sol
@@ -3,6 +3,6 @@ pragma solidity ^0.8.4;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract myToken is ERC20 {
-    
+contract MyToken is ERC20 {
+    constructor(uint256 initialSupply)
 }
