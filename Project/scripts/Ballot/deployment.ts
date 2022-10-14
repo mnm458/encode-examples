9339fdec1875fbe75d54259454f07a7fd1de7aa4
mnm458 <mohaknainmalhotra@gmail.com>
2022-10-14 19:39:10 +1100

    deployment

--- Project/scripts/Ballot/deployment.ts
+++ Project/scripts/Ballot/deployment.ts
@@ -3,7 +3,7 @@ import "dotenv/config";
 import * as ballotJson from "../../artifacts/contracts/Ballot.sol/Ballot.json"
 const EXPOSED_KEY = "8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de8f";
 
-const PROPOSALS = ["Proposal 1". "Proposal 2", "Proposal 3"];
+const PROPOSALS = ["Proposal 1", "Proposal 2", "Proposal 3"];
 
 function convertStringArrayToBytes32(array: string[]){
   const bytes32Array = [];
@@ -17,7 +17,8 @@ async function main(){
   const wallet = process.env.MNEMONIC && process.env.MNEMONIC.length > 0
     ? ethers.Wallet.fromMnemonic(process.env.MNEMONIC)
     : new ethers.Wallet(process.env.PRIVATE_KEY ?? EXPOSED_KEY);
-  console.log({wallet})
+  console.log(`Using address ${wallet.address}`)
+  const
 }
 
 main().catch((error) => {
