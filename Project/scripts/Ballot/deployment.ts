361e6e8ea9a1cac3d71d4a2c36537c91f8e9fec9
mnm458 <mohaknainmalhotra@gmail.com>
2022-10-14 19:29:05 +1100

    deployment

--- Project/scripts/Ballot/deployment.ts
+++ Project/scripts/Ballot/deployment.ts
@@ -1,4 +1,4 @@
-import {ethers} from "ethers";
+import {ContractFactory, ethers} from "ethers";
 import "dotenv/config";
 import * as ballotJson from "../../artifacts/contracts/Ballot.sol/Ballot.json"
 const EXPOSED_KEY = "8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de8f";
@@ -12,7 +12,8 @@ function convertStringArrayToBytes32(array: string[]){
 }
 
 async function main(){
-  const contractFactory = new ethers.ContractFactory(ballotJson.abi, ballotJson.bytecode)
+  const contractFactory = new ethers.ContractFactory(ballotJson.abi, ballotJson.bytecode);
+  const contract = await contractFactory.deploy(convertStringArrayToBytes32(PROPOSALS));
 }
 
 main().catch((error) => {
