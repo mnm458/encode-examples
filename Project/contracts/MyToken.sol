2a9a8f066e8b5cf17bc6a8975b31554a800568f0
mnm458 <mohaknainmalhotra@gmail.com>
2022-10-15 02:53:23 +1100

    erc20

--- Project/contracts/MyToken.sol
+++ Project/contracts/MyToken.sol
@@ -7,6 +7,7 @@ import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 contract MyToken is ERC20, AccessControl {
 
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bool myVar;
     constructor(uint256 initialSupply) ERC20("GOLD", "GLD"){
         _mint(msg.sender, initialSupply);
     }
