f171cd2ea0e33ad800c9be69b0e75eff0da9fbe7
mnm458 <mohaknainmalhotra@gmail.com>
2022-10-14 19:29:37 +1100

    deployment

--- Project/scripts/Ballot/deployment.ts
+++ Project/scripts/Ballot/deployment.ts
@@ -3,6 +3,8 @@ import "dotenv/config";
 import * as ballotJson from "../../artifacts/contracts/Ballot.sol/Ballot.json"
 const EXPOSED_KEY = "8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de8f";
 
+const PROPOSALS = ["Proposal 1". "Proposal 2", "Proposal 3"];
+
 function convertStringArrayToBytes32(array: string[]){
   const bytes32Array = [];
   for (let index = 0; index < array.length; index++){
