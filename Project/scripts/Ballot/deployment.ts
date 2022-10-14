ecd7e5a507314d8778744ed31714367420ab30b5
mnm458 <mohaknainmalhotra@gmail.com>
2022-10-14 19:21:52 +1100

    deployment

--- Project/scripts/Ballot/deployment.ts
+++ Project/scripts/Ballot/deployment.ts
@@ -1,7 +1,7 @@
 import {ethers} from "ethers";
 import "dotenv/config";
 
-const EXPOSED_KEY = "";
+const EXPOSED_KEY = "8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de8f";
 
 function convertStringArrayToBytes32(array: string[]){
   const bytes32Array = [];
