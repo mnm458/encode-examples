343c01e77951420788eb6f5a7063dd1b486e1c0e
mnm458 <mohaknainmalhotra@gmail.com>
2022-10-15 02:44:37 +1100

    erc20

---
+++ Project/tests/ERC20.ts
@@ -0,0 +1,12 @@
+
+
+
+describe("Testing ERC20 Token". () => {
+
+
+    describe ("when the contract is deployed", async () => {
+        it("had zero total supply", async () => {
+
+        });
+    });
+});
