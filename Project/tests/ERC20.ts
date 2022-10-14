0c49619b4a2ac21eaa134312fc266e71bfa3c442
mnm458 <mohaknainmalhotra@gmail.com>
2022-10-15 03:12:58 +1100

    erc20

--- Project/tests/ERC20.ts
+++ Project/tests/ERC20.ts
@@ -1,5 +1,7 @@
 
-
+import { BigNumber } from "ethers";
+import {ethers} from "hardhat";
+import {expect} from "chai";
 
 
 describe("Testing ERC20 Token", () => {
@@ -19,7 +21,11 @@ describe("Testing ERC20 Token", () => {
 
   describe("when the contract is deployed", async () => {
     it("has zero total supply", async () => {
-     
+        const tokenContractFactory = await ethers.getContractFactory("MyToken");
+        const tokenContract = await tokenContractFactory.deploy(); 
+        await tokenContract.deployed();
+        const totalSupply = await tokenContract.totalSupply();
+        expect(totalSupply).to.eq(0);
     });
 
     it("sets the deployer as minter", async () => {
